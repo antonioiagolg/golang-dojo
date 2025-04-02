f24b5546bc924e960ddd6c4285258040abd6ec34
Antonio Iago <antonio.iagolg@gmail.com>
2025-04-02 05:47:37 +0000

    Testing pointers and errors

---
+++ testing-go/pointers-errors/go.mod
@@ -0,0 +1,3 @@
+module antonioiagolg.github.io/testing-go/pointer-errors
+
+go 1.24.1
---
+++ testing-go/pointers-errors/main.go
@@ -0,0 +1,15 @@
+package main
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() (Bitcoin) {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
